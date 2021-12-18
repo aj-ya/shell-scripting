89d4d3ddea5c857a726c7ee239b1e82d78012590
aj-ya <ajeyabhat.off@gmail.com>
2021-12-18 22:32:25 +0530

    idk v9?

--- ceilometer_inp.sh
+++ ceilometer_inp.sh
@@ -7,4 +7,4 @@ OS_DOMAIN_NAME="default"
 OS_AUTH_URL='192.168.31.2/v3/'
 
 #ceilometer --os-username $OS_USERNAME --os-password $OS_PASSWORD --os-project-id $OS_PROJECT_ID --os-domain-name $OS_DOMAIN_NAME --os-auth-url $OS_AUTH_URL sample-list --meter cpu_util
-watch -n 1 "ceilometer --os-username admin --os-password admin_pass --os-project-id 2448c5574f994284a0de2604962a55a0 --os-domain-name default --os-auth-url 192.168.31.2/v3/  sample-list --meter cpu_util >> cpu_util.txt"
+watch -n 1 "ceilometer --os-username admin --os-password admin_pass --os-project-id 2448c5574f994284a0de2604962a55a0 --os-user-domain-name default --os-auth-url 192.168.31.2/v3/  sample-list --meter cpu_util >> cpu_util.txt"
